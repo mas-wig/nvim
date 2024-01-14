3b0c1c7a59f67fa32b18e4f3b031a9172cb9b66c
wigtono <mas_wig@protonmail.ch>
2024-01-15 00:04:21 +0700

    Extend harpoon and nvim-notify

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,5 +1,4 @@
 local map = vim.keymap.set
-
 if vim.lsp.get_clients({ name = "gopls" }) then
 	map("n", "<leader>jw", "<cmd>GoFillSwitch<cr>", { desc = "Fill Switch" })
 	map("n", "<leader>jf", "<cmd>GoFillStruct<cr>", { desc = "Auto Sill Struct" })
--- lua/plugins/coding/init.lua
+++ lua/plugins/coding/init.lua
@@ -9,16 +9,17 @@ return {
 					layouts = {
 						{
 							elements = {
-								{ id = "scopes", size = 0.25 },
-								{ id = "breakpoints", size = 0.25 },
-								{ id = "stacks", size = 0.25 },
-								{ id = "watches", size = 0.25 },
+								{ id = "scopes", size = 0.2 },
+								{ id = "breakpoints", size = 0.2 },
+								{ id = "stacks", size = 0.2 },
+								{ id = "watches", size = 0.2 },
+								{ id = "console", size = 0.2 },
 							},
 							position = "right",
 							size = 55,
 						},
 						{
-							elements = { { id = "repl", size = 0.5 }, { id = "console", size = 0.5 } },
+							elements = { { id = "repl", size = 1 } },
 							position = "bottom",
 							size = 8,
 						},
